12c776d121d9ed11c8ff62b109d74dddb50b80a4
automaton82 <terrence.sheflin@gmail.com>
2022-06-08 21:20:55 -0400

    Moving namespaces.

---
+++ app/src/main/java/com/systems/automaton/classtimetableplanner/appwidget/AppWidgetConstants.java
@@ -0,0 +1,11 @@
+package com.systems.automaton.classtimetableplanner.appwidget;
+
+/**
+ * From https://github.com/SubhamTyagi/TimeTable
+ */
+final class AppWidgetConstants {
+
+    static final int TIME_STYLE_FIRST = 0;
+    static final int TIME_STYLE_SECOND = 1;
+    static final int TIME_STYLE_THIRD = 2;
+}
